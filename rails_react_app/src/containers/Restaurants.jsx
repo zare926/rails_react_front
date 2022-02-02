4b9fd3a1d79823f16494aba1904c310249b6f77a
zare926 <ilove.boc926@gmail.com>
2022-02-03 08:27:25 +0900

    APIから取得したデータを描画

--- rails_react_app/src/containers/Restaurants.jsx
+++ rails_react_app/src/containers/Restaurants.jsx
@@ -1,7 +1,119 @@
-import React,{Fragment} from 'react';
+import React,{Fragment, useReducer,useEffect} from 'react';
+import { Link } from "react-router-dom";
+// api
+import {fetchRestaurants} from '../apis/restaurants'
+
+// css
+import styled from 'styled-components';
+
+// material
+import Skeleton from '@mui/material/Skeleton';
+
+
+// constants
+import { REQUEST_STATE } from '../constants';
+
+// images
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+import RestaurantImage from '../images/restaurant-image.jpg';
+
+// reducer
+import {
+  initialState,
+  restaurantsActionTyps,
+  restaurantsReducer,
+} from '../reducers/restaurants';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
+
+const RestaurantsContentsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 150px;
+`;
+
+const RestaurantsContentWrapper = styled.div`
+  width: 450px;
+  height: 300px;
+  padding: 48px;
+`;
+
+const RestaurantsImageNode = styled.img`
+  width: 100%;
+`;
+
+const MainText = styled.p`
+  color: black;
+  font-size: 18px;
+`;
+
+const SubText = styled.p`
+  color: black;
+  font-size: 12px;
+`;
 
 export const Restaurants = () => {
+
+  const [state, dispatch] = useReducer(restaurantsReducer,initialState);
+
+
+useEffect(() => {
+  dispatch({type: restaurantsActionTyps.FETCHING});
+  fetchRestaurants()
+  .then((data) => 
+  dispatch({
+    type: restaurantsActionTyps.FETCH_SUCCESS,
+    payload: {
+      restaurants: data.restaurants
+    }
+    })
+  )
+},[])
+
   return <Fragment>
-    レストラン一覧
+    <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+      <RestaurantsContentsList>
+        {
+          state.fetchState === REQUEST_STATE.LOADING ?
+            <Fragment>
+              <Skeleton variant="rect" width={450} height={300} />
+              <Skeleton variant="rect" width={450} height={300} />
+              <Skeleton variant="rect" width={450} height={300} />
+            </Fragment>
+          :
+            state.restaurantsList.map((item, index) =>
+              <Link to={`/restaurants/${item.id}/foods`} key={index} style={{ textDecoration: 'none' }}>
+                <RestaurantsContentWrapper>
+                  <RestaurantsImageNode src={RestaurantImage} />
+                  <MainText>{item.name}</MainText>
+                  <SubText>{`配送料：${item.fee}円 ${item.time_required}分`}</SubText>
+                </RestaurantsContentWrapper>
+              </Link>
+            )
+        }
+      </RestaurantsContentsList>
+      // --- ここまで追加 ---
   </Fragment>;
 };
--- rails_react_app/src/urls/index.js
+++ rails_react_app/src/urls/index.js
@@ -1,4 +1,4 @@
-const DEFAULT_API_LOCALHOST = 'https://localhost:3000/api/v1'
+const DEFAULT_API_LOCALHOST = 'http://localhost:3000/api/v1'
 
 export const restaurantsIndex = `${DEFAULT_API_LOCALHOST}/restaurants`
 export const foodsIndex = (restaurantId) => `${DEFAULT_API_LOCALHOST}/restaurants/${restaurantId}/foods`
